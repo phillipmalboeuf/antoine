bc39aaa99564f70fa1f3c6b73afbcc4871cdd322
Phillip Malboeuf <phil@phils.computer>
2022-02-08 16:46:21 -0500

    webkit fill avail

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -50,6 +50,7 @@
     z-index: 1;
     width: 100%;
     height: 100%;
+    min-height: -webkit-fill-available;
     padding: calc(var(--gutter) * 0.8) var(--gutter);
     display: flex;
     flex-direction: column;
